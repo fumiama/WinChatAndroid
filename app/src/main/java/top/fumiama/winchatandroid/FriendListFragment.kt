7806195425773511c051825ae58ca603c80bb0ee
源文雨 <41315874+fumiama@users.noreply.github.com>
2022-12-13 20:35:38 +0800

    add recv text message

--- app/src/main/java/top/fumiama/winchatandroid/FriendListFragment.kt
+++ app/src/main/java/top/fumiama/winchatandroid/FriendListFragment.kt
@@ -1,23 +1,31 @@
 package top.fumiama.winchatandroid
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.core.content.edit
 import androidx.navigation.fragment.findNavController
 import androidx.preference.PreferenceManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_friendlist.*
 import top.fumiama.winchatandroid.client.User
 import top.fumiama.winchatandroid.databinding.FragmentFriendlistBinding
+import top.fumiama.winchatandroid.ui.FriendListViewHolder
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FriendListFragment : Fragment() {
 
+    private var pref: SharedPreferences? = null
+
     private var _binding: FragmentFriendlistBinding? = null
+
+    private var ad: FriendListViewHolderInstance.RecyclerViewAdapter? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -28,13 +36,20 @@ class FriendListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentFriendlistBinding.inflate(inflater, container, false)
-        friend_lst_f_handler = FriendListFragmentHandler(this, Looper.myLooper()!!)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        ad = FriendListViewHolderInstance(ffr).RecyclerViewAdapter()
+        pref = PreferenceManager.getDefaultSharedPreferences(requireContext())
+        friendListFragmentHandler = FriendListFragmentHandler(this, Looper.myLooper()!!)
+        ffr.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = ad
+            setOnScrollChangeListener { _, _, scrollY, _, _ -> binding.ffsw.isEnabled = scrollY == 0  }
+        }
         binding.ffsw.setOnRefreshListener {
             if(user == null) { // 未登录
                 binding.root.postDelayed({
@@ -48,18 +63,74 @@ class FriendListFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+        ad = null
     }
 
     fun insertRow(b: Bundle) {
-        val name = b.getString("name", "N/A")
         val msg = b.getString("msg", "N/A")
-        val count = b.getInt("count", 0)
-        val total = b.getInt("total", 0)
+        val id = b.getInt("id", 0)
+        pref?.getString(id.toString(), "")?.let { dataStr ->
+            if(dataStr == "") { // new msg
+                val data = arrayOf("匿名", msg, "1")
+                pref?.edit {
+                    putString(id.toString(), "${data[0]}\n${data[1]}\n${data[2]}")
+                    apply()
+                }
+                try {
+                    ad?.apply {
+                        idDataMap[id] = data
+                        add(id)
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            } else {
+                val data = dataStr.split("\n").toTypedArray()
+                data[2] = (data[2].toInt()+1).toString()
+                pref?.edit {
+                    putString(id.toString(), "${data[0]}\n${data[1]}\n${data[2]}")
+                    apply()
+                }
+                try {
+                    ad?.apply {
+                        idDataMap[id] = data
+                        replace(id)
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
 
+    inner class FriendListViewHolderInstance(itemView: View): FriendListViewHolder(itemView) {
+        private val onLineClicked = { id: Int, data: Array<String>, v: View ->
+
+        }
+        private val onLineLongClicked = { id: Int, data: Array<String>, v: View ->
+
+        }
+        inner class RecyclerViewAdapter: FriendListViewHolder.RecyclerViewAdapter(onLineClicked, onLineLongClicked) {
+            val idDataMap = hashMapOf<Int, Array<String>>()
+            override fun getKeys(): List<Int> {
+                pref?.all?.forEach { e ->
+                    e.key.toIntOrNull()?.let { id ->
+                        val data = e.value.toString().split("\n").toTypedArray()
+                        if(data.size != 3) return@forEach
+                        idDataMap[id] = data
+                    }
+                }
+                return idDataMap.keys.distinct()
+            }
+
+            override fun getValue(id: Int): Array<String>? {
+                return idDataMap[id]
+            }
+        }
     }
 
     companion object {
         var user: User? = null
-        var friend_lst_f_handler: FriendListFragmentHandler? = null
+        var friendListFragmentHandler: FriendListFragmentHandler? = null
     }
 }
--- app/src/main/java/top/fumiama/winchatandroid/LoginFragment.kt
+++ app/src/main/java/top/fumiama/winchatandroid/LoginFragment.kt
@@ -1,12 +1,17 @@
 package top.fumiama.winchatandroid
 
 import android.os.Bundle
+import android.os.Message
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import top.fumiama.winchatandroid.FriendListFragment.Companion.friendListFragmentHandler
+import top.fumiama.winchatandroid.client.Command
+import top.fumiama.winchatandroid.client.Command.Companion.CMD_TYPE_MSG_TXT
+import top.fumiama.winchatandroid.client.TextMessage
 import top.fumiama.winchatandroid.client.User
 import top.fumiama.winchatandroid.databinding.FragmentLoginBinding
 
@@ -37,11 +42,30 @@ class LoginFragment : Fragment() {
         }
         binding.flb.setOnClickListener {
             Thread {
-                FriendListFragment.user = User(binding.flitun.text.toString(), binding.flitpwd.text.toString()).let {
-                    context?.let { ctx ->
-                        it.login(SettingsFragment.getUDP(ctx))
+                context?.let { ctx ->
+                    val udp = SettingsFragment.getUDP(ctx)
+                    FriendListFragment.user = User(binding.flitun.text.toString(), binding.flitpwd.text.toString()).let {
+                        it.login(udp)
+                        it
                     }
-                    it
+                    while (FriendListFragment.user != null) {
+                        FriendListFragment.user?.let userLet@ { user ->
+                            user.getCommand(udp)?.let cmdLet@ { cmd ->
+                                when(cmd.typ) {
+                                    CMD_TYPE_MSG_TXT -> {
+                                        val msgTxt = TextMessage(cmd.data)
+                                        if(msgTxt.toID != user.userID()) return@userLet
+                                        val msg = Message.obtain(friendListFragmentHandler)
+                                        val data = Bundle()
+                                        data.putInt("id", msgTxt.fromID)
+                                        data.putString("msg", msgTxt.msg)
+                                        msg.data = data
+                                        msg.sendToTarget()
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }.start()
         }
